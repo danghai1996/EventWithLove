cb55fe2e84490acfe7fc5148140ecbe89a035e48
NHEM <danghai.hy,1996@gmail.com>
2018-01-20 02:01:07 +0700

    no message

--- app/src/main/java/com/example/nhem/eventwithlove/event/activities/activities/ListEventActivity.java
+++ app/src/main/java/com/example/nhem/eventwithlove/event/activities/activities/ListEventActivity.java
@@ -1,7 +1,9 @@
-package com.example.nhem.eventwithlove;
+package com.example.nhem.eventwithlove.event.activities.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.nhem.eventwithlove.R;
 
 public class ListEventActivity extends AppCompatActivity {
 
