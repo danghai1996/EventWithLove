063e2a4ace0fd66cb3d91d0bdd6bedc0315d3b39
haucv <hauchu1196@gmail.com>
2018-01-20 00:19:06 +0700

    add login with fb

--- app/src/main/java/com/example/nhem/eventwithlove/MainActivity.java
+++ app/src/main/java/com/example/nhem/eventwithlove/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.nhem.eventwithlove;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.toString();
+    CallbackManager callbackManager = CallbackManager.Factory.create();
+    LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
     }
+
+    private void init() {
+        loginButton.setReadPermissions("email");
+//        // If using in a fragment
+//        loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "onSuccess: " + loginResult.toString());
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "onCancel: ");
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.d(TAG, "onError: " + exception.toString());
+                // App code
+            }
+
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
