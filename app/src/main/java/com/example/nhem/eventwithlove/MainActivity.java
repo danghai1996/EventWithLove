084981b4be59bb3c376017ac445c286c6798fb03
haucv <hauchu1196@gmail.com>
2018-01-20 02:01:27 +0700

    add qrcode activity

--- app/src/main/java/com/example/nhem/eventwithlove/MainActivity.java
+++ app/src/main/java/com/example/nhem/eventwithlove/MainActivity.java
@@ -5,32 +5,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphRequestAsyncTask;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.toString();
     CallbackManager callbackManager = CallbackManager.Factory.create();
+    LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email");
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
 //        // If using in a fragment
 //        loginButton.setFragment(this);
 
         // Callback registration
+        facebookLogin();
+
+    }
+
+    private void facebookLogin() {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.d(TAG, "onSuccess: " + loginResult.toString());
+                startActivity(new Intent(MainActivity.this, QRActvity.class));
+
                 // App code
             }
 
